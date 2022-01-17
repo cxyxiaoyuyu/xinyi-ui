b51a7d15c6d0287c2cd099f62f2f040eff6eeb8a
cxyxiaoyuyu.com <923996381@qq.com>
2022-01-17 16:10:33 +0800

    打包库

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { md } from './plugins/md'
 import fs from 'fs'
 import { baseParse } from '@vue/compiler-core'
 
+import path from 'path'
+
 const vueDemoPlugin = {
   name: 'vue-i18n',
   transform(code, path) {
@@ -29,6 +31,23 @@ export default defineConfig({
     alias: {
       '@': resolve(__dirname, 'src'),
       'comp': '@/components'
+    }
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib/index.ts'),
+      name: 'xinyi-ui',
+      fileName: (format) => `xinyi-ui.${format}.js`
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue'
+        }
+      }
     }
   }
 })
