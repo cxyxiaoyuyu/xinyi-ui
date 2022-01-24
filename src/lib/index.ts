ec6de93c4f5d47a2617b15aa58d4e8ea382be110
cxyxiaoyuyu.com <923996381@qq.com>
2022-01-24 12:04:58 +0800

    button Extra ButtonGroup

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,13 +1,17 @@
 import Switch from './Switch.vue'
 import Icon from './Icon.vue'
+import Button from './Button.vue'
+import ButtonGroup from './ButtonGroup.vue'
 
 import { withInstall,makeInstaller } from '../util/install'
 
 // 局部引入
 export const XSwitch = withInstall(Switch)
 export const XIcon = withInstall(Icon)
+export const XButton = withInstall(Button,[ButtonGroup])
 
-const components = [XSwitch,XIcon]
+
+const components = [XSwitch,XIcon,XButton]
 
 // 全局引入
 export default makeInstaller(components)
--- src/util/install.ts
+++ src/util/install.ts
@@ -1,6 +1,13 @@
-export const withInstall = function(comp){
+// extras 是注册该组件需要注册的额外组件 
+// 比如 button 需要注册 button-group
+export const withInstall = function(comp,extras?){
   comp.install = (app):void => {
     app.component(comp.name,comp)
+    if(extras){
+      for(let c of extras){
+        app.component(c.name,c) 
+      } 
+    }
   }
   return comp
 }
