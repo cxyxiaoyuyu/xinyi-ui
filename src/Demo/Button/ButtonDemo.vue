3a243c89107132ab7a3fe38867ba1222eadadccb
cxyxiaoyuyu.com <923996381@qq.com>
2022-01-24 19:20:31 +0800

    添加table

--- src/Demo/Button/ButtonDemo.vue
+++ src/Demo/Button/ButtonDemo.vue
@@ -19,11 +19,17 @@ import Loading from "./loading.vue";
 import Size from "./size.vue";
 export default {
   setup() {
-    const thead = ['属性','说明','类型','默认值']
+    const thead = ['属性','说明','类型','可选值','默认值']
     const data = [
-      ['type','按钮类型','string','button'],
-      ['plain','是否为朴素按钮','boolean','false'],
-      ['text','是否为文本按钮','boolean','false']
+      ['type','按钮类型','string','primary / success / warning / danger / info','button'],
+      ['plain','是否为朴素按钮','boolean','true/false','false'],
+      ['text','是否为文本按钮','boolean','true/false','false'],
+      ['round','是否为圆角按钮','boolean','true/false','false'],
+      ['icon','图标名','string','',''],
+      ['circle','是否为圆形按钮','boolean','true/false','false'],
+      ['disabled','是否disabled','boolean','true/false','false'],
+      ['loading','是否为加载中状态','boolean','true/false','false'],
+      ['size','按钮大小','boolean','big/medium/small','medium'],
     ]
     return { Type,ButtonGroup,Icon,Disabled,Size,Loading, thead, data};
   },
