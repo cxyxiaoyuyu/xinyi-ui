4b2fbcc04576e2073c52f877b22004f798968cf6
cxyxiaoyuyu.com <923996381@qq.com>
2022-01-25 10:53:10 +0800

    使用setup 语法糖

--- src/Demo/Button/ButtonDemo.vue
+++ src/Demo/Button/ButtonDemo.vue
@@ -1,6 +1,6 @@
 <template>
   <h2>Button 组件示例</h2>
-  <Demo :component="Type"></Demo>  
+  <Demo :component="Type"></Demo>
   <Demo :component="Icon"></Demo>
   <Demo :component="Disabled"></Demo>
   <Demo :component="ButtonGroup"></Demo>
@@ -10,28 +10,24 @@
   <h2>属性</h2>
   <x-table :thead="thead" :data="data"></x-table>
 </template>
-<script lang="ts">
+
+<script lang="ts" setup>
 import Type from "./type.vue";
 import Icon from "./icon.vue";
 import Disabled from "./disabled.vue";
 import ButtonGroup from "./button-group.vue";
 import Loading from "./loading.vue";
 import Size from "./size.vue";
-export default {
-  setup() {
-    const thead = ['属性','说明','类型','可选值','默认值']
-    const data = [
-      ['type','按钮类型','string','primary / success / warning / danger / info','button'],
-      ['plain','是否为朴素按钮','boolean','true/false','false'],
-      ['text','是否为文本按钮','boolean','true/false','false'],
-      ['round','是否为圆角按钮','boolean','true/false','false'],
-      ['icon','图标名','string','',''],
-      ['circle','是否为圆形按钮','boolean','true/false','false'],
-      ['disabled','是否disabled','boolean','true/false','false'],
-      ['loading','是否为加载中状态','boolean','true/false','false'],
-      ['size','按钮大小','boolean','big/medium/small','medium'],
-    ]
-    return { Type,ButtonGroup,Icon,Disabled,Size,Loading, thead, data};
-  },
-};
+const thead = ["属性", "说明", "类型", "可选值", "默认值"];
+const data = [
+  ["type", "按钮类型", "string", "primary / success / warning / danger / info", "button"],
+  ["plain", "是否为朴素按钮", "boolean", "true/false", "false"],
+  ["text", "是否为文本按钮", "boolean", "true/false", "false"],
+  ["round", "是否为圆角按钮", "boolean", "true/false", "false"],
+  ["icon", "图标名", "string", "", ""],
+  ["circle", "是否为圆形按钮", "boolean", "true/false", "false"],
+  ["disabled", "是否disabled", "boolean", "true/false", "false"],
+  ["loading", "是否为加载中状态", "boolean", "true/false", "false"],
+  ["size", "按钮大小", "boolean", "big/medium/small", "medium"],
+];
 </script>
--- src/Demo/Table/TableDemo.vue
+++ src/Demo/Table/TableDemo.vue
@@ -1,24 +1,31 @@
 <template>
- <div>
-  <h2>Table 组件示例</h2>
-  <Demo :component="Basic"></Demo>
-  <Demo :component="Stripe"></Demo>
-  <Demo :component="Border"></Demo>
- </div>
+  <div>
+    <h2>Table 组件示例</h2>
+    <Demo :component="Basic"></Demo>
+    <Demo :component="Stripe"></Demo>
+    <Demo :component="Border"></Demo>
+
+    <h2>属性</h2>
+    <x-table :thead="thead" :data="data"></x-table>
+  </div>
 </template>
 
-<script lang="ts">
-import Basic from './basic.vue'
-import Stripe from './stripe.vue'
-import Border from './border.vue'
-import { defineComponent } from 'vue'
-export default defineComponent({
-  setup(){
-    return{ Basic, Stripe, Border }
-  }
-})
+<script lang="ts" setup>
+import Basic from "./basic.vue";
+import Stripe from "./stripe.vue";
+import Border from "./border.vue";
+const thead = ["属性", "说明", "类型", "可选值", "默认值"];
+const data = [
+  ["type", "按钮类型", "string", "primary / success / warning / danger / info", "button"],
+  ["plain", "是否为朴素按钮", "boolean", "true/false", "false"],
+  ["text", "是否为文本按钮", "boolean", "true/false", "false"],
+  ["round", "是否为圆角按钮", "boolean", "true/false", "false"],
+  ["icon", "图标名", "string", "", ""],
+  ["circle", "是否为圆形按钮", "boolean", "true/false", "false"],
+  ["disabled", "是否disabled", "boolean", "true/false", "false"],
+  ["loading", "是否为加载中状态", "boolean", "true/false", "false"],
+  ["size", "按钮大小", "boolean", "big/medium/small", "medium"],
+];
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
