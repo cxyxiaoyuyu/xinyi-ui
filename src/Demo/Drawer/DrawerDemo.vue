5cc039780f91b9463d3eebe8dd516ac5fdf6f42f
cxyxiaoyuyu.com <923996381@qq.com>
2022-02-09 16:09:53 +0800

    完成drawer 组件

---
+++ src/Demo/Drawer/DrawerDemo.vue
@@ -0,0 +1,8 @@
+<template>
+  <h2>Drawer 组件示例</h2>
+  <Demo :component="Basic"></Demo>
+</template>
+
+<script lang="ts" setup>
+import Basic from "./basic.vue";
+</script>
---
+++ src/Demo/Drawer/basic.vue
@@ -0,0 +1,26 @@
+<demo>基础用法</demo>
+
+<template>
+  <div>
+    <label><input type="radio" value="top" v-model="direction" />从上往下</label>
+    <label><input type="radio" value="right" v-model="direction" />从右往左</label>
+    <label><input type="radio" value="left" v-model="direction" />从左往右</label>
+    <label><input type="radio" value="bottom" v-model="direction" />从下往上开</label>
+
+    <x-button style="margin-left:10px" @click="drawerVisible = true">open drawer</x-button>
+
+    <x-drawer title="我是标题" v-model:visible="drawerVisible" :direction="direction">
+      <span>我来啦!</span>
+    </x-drawer>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "@vue/reactivity";
+const drawerVisible = ref(false);
+const innerDrawer = ref(false);
+const direction = ref("top");
+const showDrawer = () => {
+  drawerVisible.value = true;
+};
+</script>
