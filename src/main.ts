5f7ba278fe986bdbbd1c78487463eb3478fc744c
cxyxiaoyuyu.com <923996381@qq.com>
2022-02-09 16:34:01 +0800

    注册propstable

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ import 'github-markdown-css'
 import Demo from '@/Demo/Demo.vue'
 import './index.scss'
 import xinyi from '../src/lib/index'
+import PropsTable from '../src/components/PropsTable.vue'
 
-createApp(App).use(router).component('Demo',Demo).use(xinyi).mount('#app')
+createApp(App).use(router).component('Demo',Demo).use(xinyi).component('PropsTable',PropsTable).mount('#app')
