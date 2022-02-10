a80f9c511a4b36308c0bab33ccfe4d815b7c0c3a
cxyxiaoyuyu.com <923996381@qq.com>
2022-02-10 12:41:50 +0800

    添加表格

--- src/Demo/Switch/SwitchDemo.vue
+++ src/Demo/Switch/SwitchDemo.vue
@@ -2,18 +2,17 @@
   <h2>Switch 组件示例</h2>
   <Demo :component="Basic"></Demo>
   <Demo :component="Disabled"></Demo>
+
+  <props-table :data="data"></props-table>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script lang="ts" setup>
 import Basic from "./basic.vue";
 import Disabled from "./disabled.vue";
-export default defineComponent({
-  name: "",
-  setup() {
-    return { Basic, Disabled };
-  },
-});
+const data = [
+  ["v-model","值","boolean","true/false","false"],
+  ["disabled","disabled","boolean","true/false","false"],
+]
 </script>
 
 <style lang="scss" scoped></style>
