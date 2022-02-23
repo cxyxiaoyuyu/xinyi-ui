a6bdc386566bfe96c4e40d7a5f0e39734763eb0e
cxyxiaoyuyu.com <923996381@qq.com>
2022-02-23 17:30:59 +0800

    封装slots-table组件

--- src/Demo/Dialog/basic.vue
+++ src/Demo/Dialog/basic.vue
@@ -4,8 +4,7 @@
   
   <x-dialog v-model="visible">
     <template #title>提示</template>
-    <template #content>Vue 实现了一套内容分发的 API，这套 API 的设计灵感源自 Web Components 规范草案，将slot元素作为承载分发内容的出口。
-它允许你像这样合成组件：</template>
+    <template #content>Vue 实现了一套内容分发的 API</template>
     <template #footer>
       <x-button @click="visible = false">取消</x-button>
       <x-button type="primary" @click="visible = false">确定</x-button>
---
+++ src/components/SlotsTable.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>插槽</h2>
+  <x-table :thead="thead" :data="data"></x-table>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  data: Array,
+})
+const thead = ["插槽名", "说明"];
+</script>
+
+<style lang="scss" scoped>
+h2 {
+  margin-top: 26px;
+}
+</style>
