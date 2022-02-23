cec04e6a99c099ab65e4e5ee8dff5d8e5275c93f
cxyxiaoyuyu.com <923996381@qq.com>
2022-02-23 14:52:41 +0800

    添加footer插槽

--- src/Demo/Dialog/basic.vue
+++ src/Demo/Dialog/basic.vue
@@ -1,11 +1,21 @@
 <demo>基础用法</demo>
 <template>
-<x-dialog> </x-dialog>
+  <x-button text type="primary" @click="visible = true">显示Dialog</x-button>
+  
+  <x-dialog v-model="visible">
+    <template #title>提示</template>
+    <template #content>Vue 实现了一套内容分发的 API，这套 API 的设计灵感源自 Web Components 规范草案，将slot元素作为承载分发内容的出口。
+它允许你像这样合成组件：</template>
+    <template #footer>
+      <x-button @click="visible = false">取消</x-button>
+      <x-button type="primary" @click="visible = false">确定</x-button>
+    </template>
+  </x-dialog>
 </template>
 
 <script lang="ts" setup>
+import { ref } from "vue";
+const visible = ref(false);
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
